02aef7c3e9ce156cd8a4c2712803c280d8481ae1
rana-m-h <rana29638@gmail.com>
2025-11-24 03:58:12 +0200

    fix/error: Applying enhancements

--- src/modules/subcategory/subcategory.routes.js
+++ src/modules/subcategory/subcategory.routes.js
@@ -1,22 +1,22 @@
 
 
 import { Router } from "express";
-import { addSubCategory, allSubCategory,  deleteSubCategory,  getSubCategory, updateSubCategory } from "./subcategory.Controller.js";
-import { protectedRoutes } from "../auth/auth.controller.js";
+import { addSubCategory, allSubCategory, deleteSubCategory, getSubCategory, updateSubCategory } from "./subcategory.Controller.js";
+import { protectedRoutes, allowedto } from "../auth/auth.controller.js";
 
 
-const subcategoryRouter = Router({ mergeParams:true })
-
-subcategoryRouter
-.route('/')
-.post(protectedRoutes , allowedto('admin') , addSubCategory)
-.get(allSubCategory)
+const subcategoryRouter = Router({ mergeParams: true })
 
 subcategoryRouter
-.route('/:id')
-.get(getSubCategory)
-.put(protectedRoutes , allowedto('admin') , updateSubCategory)
-.delete(protectedRoutes , allowedto('admin') , deleteSubCategory)
+    .route('/')
+    .post(protectedRoutes, allowedto('admin'), addSubCategory)
+    .get(allSubCategory)
+
+subcategoryRouter
+    .route('/:id')
+    .get(getSubCategory)
+    .put(protectedRoutes, allowedto('admin'), updateSubCategory)
+    .delete(protectedRoutes, allowedto('admin'), deleteSubCategory)
 
 
 export default subcategoryRouter
