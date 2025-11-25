df917d97fdd90ece09c39ae8a165f55a4b94db0a
rana-m-h <rana29638@gmail.com>
2025-11-25 19:05:18 +0200

    Applying enhancements

--- database/db.Connection.js
+++ database/db.Connection.js
@@ -1,7 +1,22 @@
 
 
-import mongoose from "mongoose"
+// import mongoose from "mongoose"
 
- export const dbConnection =  mongoose.connect('mongodb://127.0.0.1:27017/ECommerce').then(()=>{
-    console.log('ok')
-})
+//  export const dbConnection =  mongoose.connect('mongodb://127.0.0.1:27017/ECommerce').then(()=>{
+//     console.log('ok')
+// })
+import mongoose from "mongoose";
+
+export const dbConnection = async () => {
+    try {
+        await mongoose.connect(process.env.DB_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            serverSelectionTimeoutMS: 30000, // زيادة وقت الانتظار
+        });
+        console.log("MongoDB connected successfully");
+    } catch (error) {
+        console.error("MongoDB connection failed:", error);
+        process.exit(1); // لو فشل الاتصال، الخروج من التطبيق
+    }
+};
--- server.js
+++ server.js
@@ -1,23 +1,24 @@
 
 import express from 'express'
 import { dbConnection } from './database/db.Connection.js'
-import {bootstrap} from "./src/modules/bootstrap.js"
+import { bootstrap } from "./src/modules/bootstrap.js"
 import { globalError } from './src/modules/middelware/globalError.js'
 import { AppError } from './src/utilts/appError.js'
 import 'dotenv/config'
 const app = express()
 const port = 3000
 
- app.use(express.json())
- app.use('/uploads' , express.static('uploads'))
+app.use(express.json())
+app.use('/uploads', express.static('uploads'))
+await dbConnection(); // أهم خطوة قبل bootstrap
 
 bootstrap(app)
 
-app.use('*' ,(req , res , next)=>{
-    next(new AppError(`route not found ${req.originalUrl}` , 404))
-  })
-  
-  
+app.use('*', (req, res, next) => {
+  next(new AppError(`route not found ${req.originalUrl}`, 404))
+})
+
+
 app.use(globalError)
 
 
