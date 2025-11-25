d67d970462bd9ecf84947857739db93873326846
rana-m-h <rana29638@gmail.com>
2025-11-26 00:24:09 +0200

    Applying enhancements

--- database/db.Connection.js
+++ database/db.Connection.js
@@ -2,7 +2,6 @@
 
 import mongoose from "mongoose"
 
-export const dbConnection = mongoose.connect(process.env.DB_URL).then(() => {
+export const dbConnection = mongoose.connect('mongodb://127.0.0.1:27017/ECommerce').then(() => {
     console.log('ok')
 })
-// 'mongodb://127.0.0.1:27017/ECommerce'
