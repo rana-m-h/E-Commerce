91f6aa70220756e825fcba7c9dd987a59e93a9cb
rana-m-h <rana29638@gmail.com>
2025-11-26 01:54:06 +0200

    Applying enhancements

--- database/db.Connection.js
+++ database/db.Connection.js
@@ -1,8 +1,14 @@
 
 
-import mongoose from "mongoose"
+// import mongoose from "mongoose"
 
-export const dbConnection = mongoose.connect(process.env.UPSTASH_REDIS_REST_URL).then(() => {
-    console.log('ok')
-})
-// 'mongodb://127.0.0.1:27017/ECommerce'
+// export const dbConnection = mongoose.connect(process.env.UPSTASH_REDIS_REST_URL).then(() => {
+//     console.log('ok')
+// })
+// // 'mongodb://127.0.0.1:27017/ECommerce'
+import { Redis } from '@upstash/redis';
+
+const redis = new Redis({
+    url: process.env.UPSTASH_REDIS_URL,
+    token: process.env.UPSTASH_REDIS_TOKEN
+});
