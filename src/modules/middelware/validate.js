f924fb8a101ae109f52693579a45dfae9b937663
rana-m-h <rana29638@gmail.com>
2025-11-30 01:18:17 +0200

    Applying enhancements

--- src/modules/middelware/validate.js
+++ src/modules/middelware/validate.js
@@ -1,19 +1,41 @@
-import { AppError } from "../../utilts/appError.js"
+// import { AppError } from "../../utilts/appError.js"
 
 
-export const validate = (schema)=>{
-return (req , res , next)=>{
-    let {error} = schema.validate({image: req.file,...req.body , ...req.params , ...req.query}, {abortEarly:false})
-    if(!error){
+// export const validate = (schema)=>{
+// return (req , res , next)=>{
 
-        next()
+//     let {error} = schema.validate({image: req.file,...req.body , ...req.params , ...req.query}, {abortEarly:false})
+//     if(!error){
 
-    }
-    else {
-        let errMessage = error.details.map(err =>  err.message)
-         next(new AppError(errMessage , 401))
-    
-    
-     }
-}
-}
+//         next()
+
+//     }
+//     else {
+//         let errMessage = error.details.map(err =>  err.message)
+//          next(new AppError(errMessage , 401))
+
+
+//      }
+// }
+// } 
+import { AppError } from "../../utilts/appError.js";
+
+export const validate = (schema) => {
+    return (req, res, next) => {
+        // نجمع البيانات من body, params, query
+        const dataToValidate = { ...req.body, ...req.params, ...req.query };
+
+        // نضيف image لو موجود
+        if (req.file) {
+            dataToValidate.image = req.file;
+        }
+
+        const { error } = schema.validate(dataToValidate, { abortEarly: false });
+
+        if (!error) return next();
+
+        // نجمع كل رسائل الأخطاء في array
+        const errMessage = error.details.map(err => err.message);
+        next(new AppError(errMessage, 401));
+    };
+};
