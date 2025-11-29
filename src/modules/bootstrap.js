f520c287589e7327045494780602e0afe8be9de0
rana-m-h <rana29638@gmail.com>
2025-11-29 23:57:50 +0200

    Applying enhancements

--- src/modules/bootstrap.js
+++ src/modules/bootstrap.js
@@ -6,6 +6,7 @@ import reviewRouter from "./review/review.routes.js"
 import subcategoryRouter from "./subcategory/subcategory.routes.js"
 import userRouter from "./user/user.routes.js"
 import wishlistRouter from "./wishlist/wishlist.routes.js"
+import authRouter from "./auth/auth.routes.js"
 
 
 export const bootstrap = (app) => {
@@ -14,7 +15,7 @@ export const bootstrap = (app) => {
     app.use('/api/brands', brandRouter)
     app.use('/api/products', productRouter)
     app.use('/api/users', userRouter)
-    app.use('/api/auth', userRouter)
+    app.use('/api/auth', authRouter)
     app.use('/api/reviews', reviewRouter)
     app.use('/api/wishlists', wishlistRouter)
     app.use('/api/addresses', addressRouter)
