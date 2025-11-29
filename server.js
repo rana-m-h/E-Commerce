3995b1c0a29f11953c8d85906f81719a7a10fd6e
rana-m-h <rana29638@gmail.com>
2025-11-30 01:06:30 +0200

    Applying enhancements

--- server.js
+++ server.js
@@ -19,16 +19,7 @@ app.use('*', (req, res, next) => {
 
 
 app.use(globalError)
-app.post('/signup', (req, res, next) => {
-  console.log('Request Body:', req.body)  // هنا هتشوف كل الحقول اللي جايه
 
-  const { error } = singUpVal.validate(req.body)
-  if (error) {
-    return next(new AppError(error.details.map(d => d.message).join(','), 400))
-  }
-
-  res.send('Signup data is valid!')
-})
 
 app.get('/', (req, res) => res.send('Hello World!'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- src/modules/auth/auth.vaildation.js
+++ src/modules/auth/auth.vaildation.js
@@ -3,26 +3,26 @@
 import joi from 'joi'
 
 
- const singUpVal = joi.object({
+const singUpVal = joi.object({
 
 
-name:joi.string().min(2).max(20).required(),
-email: joi.string().email().required(),
-password: joi.string().pattern(/^[A-Z][A-Za-z0-9]{8,40}$/).required(),
-rePassword:joi.valid(joi.ref('password')).required(),
-role: joi.string().required()
+    name: joi.string().min(2).max(20).required(),
+    email: joi.string().email().required(),
+    password: joi.string().pattern(/^[A-Z][A-Za-z0-9]{8,40}$/).required(),
+    rePassword: joi.valid(joi.ref('password')).required(),
+    role: joi.string().required()
 })
 
 
 
 const singInVal = joi.object({
-email: joi.string().email().required(),
-password: joi.string().pattern(/^[A-Z][A-Za-z0-9]{8,40}$/).required(),
+    email: joi.string().email().required(),
+    password: joi.string().pattern(/^[A-Z][A-Za-z0-9]{8,40}$/).required(),
 })
 
 
 
-export{
-singInVal,
-singUpVal
+export {
+    singInVal,
+    singUpVal
 }
