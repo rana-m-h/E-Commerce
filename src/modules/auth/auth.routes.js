89d567bd6a733e34c8b1c88bc736277ab1930dff
rana-m-h <rana29638@gmail.com>
2025-11-30 00:16:13 +0200

    Applying enhancements

--- src/modules/auth/auth.routes.js
+++ src/modules/auth/auth.routes.js
@@ -1,17 +1,17 @@
 
 
 import { Router } from "express";
-import { changePassword, singin, singup} from "./auth.controller.js";
+import { changePassword, singin, singup } from "./auth.controller.js";
 import { checkEmail } from "../middelware/checkEmail.js";
 import { validate } from "../middelware/validate.js";
-import { singInVal, singUpVal} from "./user.vaildation.js";
+import { singInVal, singUpVal } from "./auth.vaildation.js";
 
 
 const authRouter = Router()
 
 
-authRouter.post('/singup', validate(singUpVal) , checkEmail, singup)
-authRouter.post('/singin', validate(singInVal),singin)
+authRouter.post('/singup', validate(singUpVal), checkEmail, singup)
+authRouter.post('/singin', validate(singInVal), singin)
 authRouter.patch('/changePassword', changePassword)
 
 
