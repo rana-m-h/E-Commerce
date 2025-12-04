9308456f2cc572db58f235073efa98329d9e4fe5
rana-m-h <rana29638@gmail.com>
2025-12-04 23:38:53 +0200

    Applying enhancements

--- src/modules/category/category.vaildation.js
+++ src/modules/category/category.vaildation.js
@@ -5,6 +5,7 @@ import joi from 'joi'
 const addCategoryVal = joi.object({
 
     name: joi.string().min(1).max(50).required(),
+    image: joi.string().uri() // <-- السماح بحقل الصورة كـ URL
 
 })
 
