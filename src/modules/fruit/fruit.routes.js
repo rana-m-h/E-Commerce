25a2929e38251f6c7345d62c91a128b4bc3d790a
rana-m-h <rana29638@gmail.com>
2025-12-14 01:36:28 +0200

    Applying enhancements

--- src/modules/fruit/fruit.routes.js
+++ src/modules/fruit/fruit.routes.js
@@ -3,17 +3,17 @@
 import { Router } from "express";
 import { uploadSingleImage } from "../../fileUploas/upload.js";
 import { validate } from "../middelware/validate.js";
-import subcategoryRouter from "../detailsfruit/detailsfruit.routes.js";
 import { protectedRoutes, allowedto } from "../auth/auth.controller.js";
 import { addFruitVal } from "./fruit.vaildation.js";
 import { addFruit, allFruit, deleteFruit, getFruit, updateFruit } from "./fruit.Controller.js";
+import detailsfruitRouter from "../detailsfruit/detailsfruit.routes.js";
 
 
 
 const fruitRouter = Router()
 
 
-fruitRouter.use('/:fruit/subCategories', subcategoryRouter)
+fruitRouter.use('/:fruit/detailsfruits', detailsfruitRouter)
 
 fruitRouter
     .route('/')
