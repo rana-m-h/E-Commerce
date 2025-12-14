94b8fbe55c0e332d7ecd7a13843413125cac538c
rana-m-h <rana29638@gmail.com>
2025-12-14 03:24:23 +0200

    Applying enhancements

--- database/models/detailsfruit.model.js
+++ database/models/detailsfruit.model.js
@@ -4,20 +4,20 @@ import { Schema, Types, model } from "mongoose"
 
 const schema = new Schema({
 
-    // name: {
-    //     type: String,
-    //     unique: [true, 'name is required'],
-    //     trim: true,
-    //     required: true,
-    //     minLength: [2, 'too short category name']
-    // },
+    name: {
+        type: String,
+        unique: [true, 'name is required'],
+        trim: true,
+        required: true,
+        minLength: [2, 'too short category name']
+    },
 
-    // slug: {
-    //     type: String,
-    //     lowercase: true,
-    //     required: true,
-    //     unique: [true, 'name is required'],
-    // },
+    slug: {
+        type: String,
+        lowercase: true,
+        required: true,
+        unique: [true, 'name is required'],
+    },
 
     Fruit: {
 
